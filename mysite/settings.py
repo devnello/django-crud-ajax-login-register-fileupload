5c5604c9fc44543628ece7f4e8577d5d56e6fecd
Devnello <marcello.spedicato@gmail.comm>
2021-12-08 21:18:35 +0100

    Modified requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+} 
+'''
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'local',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'HOST': 'localhost',
+            'PORT': '10034',
+        }
+    }
 
 
 # Password validation
